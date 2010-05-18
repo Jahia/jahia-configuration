7deb561cbf0661af2b5a323271c6f8ff5eef5f00
Sergiy Shyrkov <sshyrkov@jahia.com>
2010-05-18 13:34:37 +0000

    git-svn-id: file:///home/svn/repository/maven-jahia-plugin/trunk@55 2b131f5d-00a7-48c4-9630-96da95fee753

---
+++ maven-jahia-plugin/src/test/java/org/jahia/utils/maven/plugin/configurators/AbstractConfiguratorTestCase.java
@@ -0,0 +1,77 @@
+package org.jahia.utils.maven.plugin.configurators;
+
+import junit.framework.TestCase;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Properties;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Common abstract class for setting up all the stuff that is needed to test configurators, such as resource paths,
+ * Oracle and MySQL configurations, default properties, etc...
+ *
+ * @author loom
+ *         Date: Oct 27, 2009
+ *         Time: 9:04:52 AM
+ */
+public abstract class AbstractConfiguratorTestCase extends TestCase {
+
+    Properties oracleDBProperties = new Properties();
+    Properties mysqlDBProperties = new Properties();
+
+    JahiaConfigBean websphereOracleConfigBean;
+    JahiaConfigBean tomcatMySQLConfigBean;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        oracleDBProperties = new Properties();
+        oracleDBProperties.load(this.getClass().getClassLoader().getResourceAsStream("configurators/WEB-INF/var/db/oracle.script"));
+        mysqlDBProperties = new Properties();
+        mysqlDBProperties.load(this.getClass().getClassLoader().getResourceAsStream("configurators/WEB-INF/var/db/mysql.script"));
+
+        URL configuratorsResourceURL = this.getClass().getClassLoader().getResource("configurators");
+        File configuratorsFile = new File(configuratorsResourceURL.toURI());
+
+        websphereOracleConfigBean = new JahiaConfigBean();
+        websphereOracleConfigBean.setDatabaseType("oracle");
+        websphereOracleConfigBean.setTargetServerType("was");
+        websphereOracleConfigBean.setTargetServerVersion("6.1.0.25");
+        websphereOracleConfigBean.setTargetConfigurationDirectory(configuratorsFile.toString());
+        websphereOracleConfigBean.setSourceWebAppDir(configuratorsFile.toString());
+        websphereOracleConfigBean.setCluster_activated("true");
+        websphereOracleConfigBean.setCluster_node_serverId("jahiaServer1");
+        websphereOracleConfigBean.setLocalIp("1.2.3.4");
+        websphereOracleConfigBean.setLocalPort("9080");
+        websphereOracleConfigBean.setProcessingServer("true");
+        List<String> clusterNodes = new ArrayList<String>();
+        clusterNodes.add("2.3.4.5");
+        clusterNodes.add("3.4.5.6");
+        clusterNodes.add("4.5.6.7");
+        websphereOracleConfigBean.setClusterNodes(clusterNodes);
+
+        tomcatMySQLConfigBean = new JahiaConfigBean();
+        tomcatMySQLConfigBean.setDatabaseType("mysql");
+        tomcatMySQLConfigBean.setTargetServerType("tomcat");
+        tomcatMySQLConfigBean.setTargetServerVersion("5.5");
+        tomcatMySQLConfigBean.setTargetConfigurationDirectory(configuratorsFile.toString());
+        tomcatMySQLConfigBean.setSourceWebAppDir(configuratorsFile.toString());
+        tomcatMySQLConfigBean.setCluster_activated("false");
+        tomcatMySQLConfigBean.setCluster_node_serverId("jahiaServer1");
+        tomcatMySQLConfigBean.setLocalIp("localhost");
+        tomcatMySQLConfigBean.setLocalPort("8080");
+        tomcatMySQLConfigBean.setProcessingServer("true");
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        
+    }
+
+    public abstract void testUpdateConfiguration() throws Exception;
+
+}
