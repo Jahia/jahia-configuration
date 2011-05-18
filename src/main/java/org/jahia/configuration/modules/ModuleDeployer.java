1a2dcda7d44ba79ce59a54bda33e891672d22cf1
Thomas Draier <tdraier@jahia.com>
2011-05-18 17:14:28 +0000

    use standalone jar for both configuration and module deployement
    git-svn-id: file:///home/svn/repository/maven-jahia-plugin/trunk@182 2b131f5d-00a7-48c4-9630-96da95fee753

--- src/main/java/org/jahia/configuration/modules/ModuleDeployer.java
+++ src/main/java/org/jahia/configuration/modules/ModuleDeployer.java
@@ -14,13 +14,6 @@ import java.util.Enumeration;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
-/**
- * Created by IntelliJ IDEA.
- * User: toto
- * Date: 5/18/11
- * Time: 4:58 PM
- * To change this template use File | Settings | File Templates.
- */
 public class ModuleDeployer {
     private File output;
     private AbstractLogger logger;
@@ -30,46 +23,11 @@ public class ModuleDeployer {
         this.logger = logger;
     }
 
-    public static void main(String[] args) {
-        ConsoleLogger logger = new ConsoleLogger();
-
-        if (args.length < 2) {
-            logger.error("Usages : ModuleDeployer module.jar [module.jar ..] jahia-root");
-            System.exit(-1);
-        }
-
-        String target = args[args.length-1];
-
-        File output = new File(target, "WEB-INF/var/shared_modules");
-
-        if (!output.exists()) {
-            logger.error("Target does not seem to be a valid jahia root folder.");
-            System.exit(-1);
-        }
-
-        ModuleDeployer deployer = new ModuleDeployer(output, logger);
-
-        try {
-            for (int i = 0; i < args.length - 1; i++) {
-                String arg = args[i];
-                File f = new File(arg);
-                if (f.exists()) {
-                    deployer.deployModule(f);
-                } else {
-                    logger.error("Cannot find file : "+f.getName());
-                }
-            }
-        } catch (IOException e) {
-            logger.error("", e);
-        }
-
-    }
-
     public void deployModule(File file) throws IOException {
         logger.info("Copy modules JAR " + file.getName() + " to shared modules folder");
         FileUtils.copyFileToDirectory(file, output);
-        copyJars(file, new File(output,"../.."));
-        copyDbScripts(file, new File(output,"../.."));
+        copyJars(file, new File(output,"../../.."));
+        copyDbScripts(file, new File(output,"../../.."));
     }
 
     private void copyJars(File warFile, File targetDir) {
@@ -100,7 +58,7 @@ public class ModuleDeployer {
                 }
             }
             if (deployed > 0) {
-                logger.info("Copied " + deployed + " JARs from " + warFile.getName() + " to jahia/WEB-INF/lib");
+                logger.info("Copied " + deployed + " JARs from " + warFile.getName() + " to WEB-INF/lib");
             }
         } catch (IOException e) {
             logger.error("Error copying JAR files for module " + warFile, e);
