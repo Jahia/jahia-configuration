9aff620a70256e7958a43b3b3ca71bd3719f7194
Guillaume Lucazeau <glucazeau@jahia.com>
2012-07-31 21:00:55 +0000

    Add random use of generated user for tasks' assignee
    git-svn-id: file:///home/svn/repository/maven-jahia-plugin/trunk@385 2b131f5d-00a7-48c4-9630-96da95fee753

--- maven-jahia-plugin/src/main/java/org/jahia/utils/maven/plugin/contentgenerator/wise/DocspaceService.java
+++ maven-jahia-plugin/src/main/java/org/jahia/utils/maven/plugin/contentgenerator/wise/DocspaceService.java
@@ -3,7 +3,6 @@ package org.jahia.utils.maven.plugin.contentgenerator.wise;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jahia.utils.maven.plugin.contentgenerator.FileService;
 import org.jahia.utils.maven.plugin.contentgenerator.bo.ExportBO;
 import org.jahia.utils.maven.plugin.contentgenerator.wise.bo.DocspaceBO;
 import org.jahia.utils.maven.plugin.contentgenerator.wise.bo.FolderBO;
@@ -33,16 +32,14 @@ private static DocspaceService instance;
 		List<NoteBO> notes = noteService.generateNotes(wiseExport.getNbNotes());
 		
 		TaskService taskService = TaskService.getInstance();
-		List<TaskBO> tasks = taskService.generateTasks(wiseExport.getNbTasks());
+		List<TaskBO> tasks = taskService.generateTasks(wiseExport.getNbTasks(), wiseExport.getNumberOfUsers());
 		
 		FileAndFolderService fileAndFolderService = FileAndFolderService.getInstance();
 		
 		List<DocspaceBO> docspaces = new ArrayList<DocspaceBO>();
 		for (int i = 1; i <= wiseExport.getNbDocspaces(); i++) {
 			String docspaceName = "docspace" + i;
-			
-			List fileNames = wiseExport.getFileNames();
-			
+						
 			List<FolderBO> folders = fileAndFolderService.generateFolders(wiseExport.getFoldersDepth(), wiseExport.getNbFoldersPerLevel(), wiseExport.getNbFilesPerFolder(), 
 					wiseExport.getFileNames(), wiseExport.getOutputDir() + "/wise", wiseExport.getWiseInstanceKey(), docspaceName, wiseExport.getFilesDirectory());
 					
