1a4b70e21e2d449935ff94aff03f6615b4362c04
Guillaume Lucazeau <glucazeau@jahia.com>
2012-08-10 14:47:02 +0000

    Add group Element
    git-svn-id: file:///home/svn/repository/maven-jahia-plugin/trunk@411 2b131f5d-00a7-48c4-9630-96da95fee753

--- maven-jahia-plugin/src/main/java/org/jahia/utils/maven/plugin/contentgenerator/wise/bo/WiseBO.java
+++ maven-jahia-plugin/src/main/java/org/jahia/utils/maven/plugin/contentgenerator/wise/bo/WiseBO.java
@@ -14,33 +14,35 @@ import org.jdom.Element;
 
 public class WiseBO extends SiteBO {
 	Element wiseElement;
-	
+
 	String wiseInstanceName;
-	
+
 	List<DocspaceBO> docspaces;
-	
+
 	public WiseBO(String wiseInstanceKey, List<DocspaceBO> docspaces) {
 		this.setSiteKey(wiseInstanceKey);
 		this.docspaces = docspaces;
 	}
-	
+
 	public Element getElement() {
 		if (wiseElement == null) {
 			wiseElement = new Element(this.getSiteKey());
 			wiseElement.setAttribute("mixinTypes", "jmix:accessControlled", ContentGeneratorCst.NS_JCR);
 			wiseElement.setAttribute("primaryType", "jnt:virtualsite", ContentGeneratorCst.NS_JCR);
+
+			wiseElement.addContent(getGroupsElement());
 			
 			Element filesElement = new Element("files");
 			filesElement.setAttribute("primaryType", "jnt:folder", ContentGeneratorCst.NS_JCR);
 			filesElement.setAttribute("publicationStatus", "3", ContentGeneratorCst.NS_J);
-					
+
 			filesElement.addContent(getContributedElement());
-			
+
 			if (CollectionUtils.isNotEmpty(docspaces)) {
 				Element docspacesElement = new Element("docspaces");
 				docspacesElement.setAttribute("mixinTypes", "jmix:workflowRulesable", ContentGeneratorCst.NS_JCR);
 				docspacesElement.setAttribute("primaryType", "jnt:folder", ContentGeneratorCst.NS_JCR);
-				
+
 				for (Iterator<DocspaceBO> iterator = docspaces.iterator(); iterator.hasNext();) {
 					DocspaceBO docspace = iterator.next();
 					docspacesElement.addContent(docspace.getElement());
@@ -51,44 +53,67 @@ public class WiseBO extends SiteBO {
 		}
 		return wiseElement;
 	}
-	
-	public Document getDocument() {
-        Document doc = new Document();
-        Element contentNode = new Element("content");
-        contentNode.addNamespaceDeclaration(ContentGeneratorCst.NS_JCR);
-        contentNode.addNamespaceDeclaration(ContentGeneratorCst.NS_JNT);
-        contentNode.addNamespaceDeclaration(ContentGeneratorCst.NS_JMIX);
-        contentNode.addNamespaceDeclaration(ContentGeneratorCst.NS_J);
-        contentNode.addNamespaceDeclaration(ContentGeneratorCst.NS_NT);
-        contentNode.addNamespaceDeclaration(ContentGeneratorCst.NS_SV);
-        contentNode.addNamespaceDeclaration(ContentGeneratorCst.NS_MIX);
-        contentNode.addNamespaceDeclaration(ContentGeneratorCst.NS_DOCNT);
-        
-        doc.setRootElement(contentNode);
 
-        Element sitesNode = new Element("sites");
-        sitesNode.setAttribute("primaryType", "jnt:virtualsitesFolder", ContentGeneratorCst.NS_JCR);
-        sitesNode.addContent(this.getElement());
-        
-        contentNode.addContent(sitesNode);
-        return doc;
+	public Document getDocument() {
+		Document doc = new Document();
+		Element contentNode = new Element("content");
+		contentNode.addNamespaceDeclaration(ContentGeneratorCst.NS_JCR);
+		contentNode.addNamespaceDeclaration(ContentGeneratorCst.NS_JNT);
+		contentNode.addNamespaceDeclaration(ContentGeneratorCst.NS_JMIX);
+		contentNode.addNamespaceDeclaration(ContentGeneratorCst.NS_J);
+		contentNode.addNamespaceDeclaration(ContentGeneratorCst.NS_NT);
+		contentNode.addNamespaceDeclaration(ContentGeneratorCst.NS_SV);
+		contentNode.addNamespaceDeclaration(ContentGeneratorCst.NS_MIX);
+		contentNode.addNamespaceDeclaration(ContentGeneratorCst.NS_DOCNT);
+
+		doc.setRootElement(contentNode);
+
+		Element sitesNode = new Element("sites");
+		sitesNode.setAttribute("primaryType", "jnt:virtualsitesFolder", ContentGeneratorCst.NS_JCR);
+		sitesNode.addContent(this.getElement());
+
+		contentNode.addContent(sitesNode);
+		return doc;
 	}
-	
+
 	private Element getContributedElement() {
 		Element contributedElement = new Element("contributed");
 		contributedElement.setAttribute("originWS", "default", ContentGeneratorCst.NS_J);
 		contributedElement.setAttribute("createdBy", "system", ContentGeneratorCst.NS_JCR);
 		contributedElement.setAttribute("mixinTypes", "jmix:accessControlled", ContentGeneratorCst.NS_JCR);
 		contributedElement.setAttribute("primaryType", "jnt:folder", ContentGeneratorCst.NS_JCR);
-	
+
 		AceBO sitePrivileged = new AceBO("site-privileged", "privileged", "g", "GRANT", "contributor");
-		
+
 		List<AceBO> aces = new ArrayList<AceBO>();
 		aces.add(sitePrivileged);
 		AclBO acl = new AclBO(aces);
-		
+
 		Element aclElement = acl.getElement();
 		contributedElement.addContent(aclElement);
 		return contributedElement;
+	}
+
+	private Element getGroupsElement() {
+		Element groupElement = new Element("groups");
+		groupElement.setAttribute("primaryType", "jnt:groupsFolder", ContentGeneratorCst.NS_JCR);
+
+		Element sitePrivileged = new Element("site-privileged");
+		sitePrivileged.setAttribute("external", "false", ContentGeneratorCst.NS_J);
+		sitePrivileged.setAttribute("hidden", "false", ContentGeneratorCst.NS_J);
+		sitePrivileged.setAttribute("mixinTypes", "jmix:systemNode", ContentGeneratorCst.NS_JCR);
+		sitePrivileged.setAttribute("primaryType", "jnt:group", ContentGeneratorCst.NS_JCR);
+
+		Element membersElement = new Element("members", ContentGeneratorCst.NS_J);
+		membersElement.setAttribute("primaryType", "jnt:members", ContentGeneratorCst.NS_JCR);
+
+		Element siteAdministratorsElement = new Element("site-administrators___3");
+		siteAdministratorsElement.setAttribute("primaryType", "jnt:member", ContentGeneratorCst.NS_JCR);
+		siteAdministratorsElement.setAttribute("member", "/sites/" + this.getSiteKey() + "/groups/site-administrators", ContentGeneratorCst.NS_J);
+
+		membersElement.setContent(siteAdministratorsElement);
+		sitePrivileged.setContent(membersElement);
+		groupElement.setContent(sitePrivileged);
+		return groupElement;
 	}
 }
