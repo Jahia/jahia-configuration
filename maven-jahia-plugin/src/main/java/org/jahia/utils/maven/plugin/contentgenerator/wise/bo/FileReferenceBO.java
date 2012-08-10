d981bcde5c6abc13e92ab40fbf4a4c25b4620e4c
Guillaume Lucazeau <glucazeau@jahia.com>
2012-08-10 15:45:29 +0000

    Fix file reference nodes
    git-svn-id: file:///home/svn/repository/maven-jahia-plugin/trunk@415 2b131f5d-00a7-48c4-9630-96da95fee753

--- maven-jahia-plugin/src/main/java/org/jahia/utils/maven/plugin/contentgenerator/wise/bo/FileReferenceBO.java
+++ maven-jahia-plugin/src/main/java/org/jahia/utils/maven/plugin/contentgenerator/wise/bo/FileReferenceBO.java
@@ -4,17 +4,21 @@ import org.jahia.utils.maven.plugin.contentgenerator.properties.ContentGenerator
 import org.jdom.Element;
 
 public class FileReferenceBO extends FileBO {
+	private Element fileReferenceElement;
 	
 	public FileReferenceBO(FileBO file) {
 		super(file.getFileName(), file.getNodePath(), file.getCreator(), file.getOwner(), file.getEditor(), file.getReader());
 	}
 
 	public Element getElement() {
-		if (fileElement == null) {
-			super.getElement();
+		if (fileReferenceElement == null) {
+			fileName =  org.apache.jackrabbit.util.ISO9075.encode(fileName);
+			fileReferenceElement = new Element(fileName);
+			
+			fileReferenceElement.setAttribute("primaryType", "jnt:fileReference", ContentGeneratorCst.NS_JCR);
+			fileReferenceElement.setAttribute("node", super.nodePath, ContentGeneratorCst.NS_J);
+			fileReferenceElement.setAttribute("originWS", "default", ContentGeneratorCst.NS_J);
 		}
-		fileElement.setAttribute("primaryType", "jnt:fileReference", ContentGeneratorCst.NS_JCR);
-		fileElement.setAttribute("node", super.nodePath, ContentGeneratorCst.NS_J);
-		return fileElement;
+		return fileReferenceElement;
 	}
 }
