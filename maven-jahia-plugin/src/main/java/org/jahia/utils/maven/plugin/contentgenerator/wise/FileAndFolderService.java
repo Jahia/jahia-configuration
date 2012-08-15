7aeeccca1354649b9945dfdc2fe9080f59b46afa
Guillaume Lucazeau <glucazeau@jahia.com>
2012-08-15 15:21:36 +0000

    Put back the file creation in its own folder
    git-svn-id: file:///home/svn/repository/maven-jahia-plugin/trunk@440 2b131f5d-00a7-48c4-9630-96da95fee753

--- maven-jahia-plugin/src/main/java/org/jahia/utils/maven/plugin/contentgenerator/wise/FileAndFolderService.java
+++ maven-jahia-plugin/src/main/java/org/jahia/utils/maven/plugin/contentgenerator/wise/FileAndFolderService.java
@@ -144,7 +144,10 @@ public class FileAndFolderService {
 			for (Iterator<FileBO> iterator = files.iterator(); iterator.hasNext();) {
 				FileBO fileBO = (FileBO) iterator.next();
 				File sourceFile = new File(filesDirectory + sep + fileBO.getFileName());
-				File targetFile = new File(newFolder + sep + fileBO.getFileName());
+				File targetDirectory = new File(newFolder + sep + fileBO.getFileName());
+				// each file is contained in its own directory
+				targetDirectory.mkdir();
+				File targetFile = new File(targetDirectory + sep + fileBO.getFileName());
 				try {
 					FileUtils.copyFile(sourceFile, targetFile);
 				} catch (IOException e) {
