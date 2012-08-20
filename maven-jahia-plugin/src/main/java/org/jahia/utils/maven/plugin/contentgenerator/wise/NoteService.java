b3c8df4f4d38f8658ecfe4065ddafbf5d2ef8d09
Guillaume Lucazeau <glucazeau@jahia.com>
2012-08-20 20:29:56 +0000

    Note notes if no users generated
    git-svn-id: file:///home/svn/repository/maven-jahia-plugin/trunk@472 2b131f5d-00a7-48c4-9630-96da95fee753

--- maven-jahia-plugin/src/main/java/org/jahia/utils/maven/plugin/contentgenerator/wise/NoteService.java
+++ maven-jahia-plugin/src/main/java/org/jahia/utils/maven/plugin/contentgenerator/wise/NoteService.java
@@ -13,9 +13,9 @@ import org.jahia.utils.maven.plugin.contentgenerator.wise.bo.NoteBO;
 
 public class NoteService {
 	private static NoteService instance;
-	
+
 	private Log logger = new SystemStreamLog();
-	
+
 	private NoteService() {
 
 	}
@@ -26,27 +26,31 @@ public class NoteService {
 		}
 		return instance;
 	}
-	
+
 	public List<NoteBO> generateNotes(Integer nbNotes, Integer nbUsers, List<ArticleBO> articles) {
 		List<NoteBO> notes = new ArrayList<NoteBO>();
-		
+
 		ArticleService articleService = ArticleService.getInstance();
-		
+
 		String creator = "root";
 		int idCreator;
 		Random rand = new Random();
-		for (int i = 1; i <= nbNotes; i++) {
-			logger.info("Generating note " + i + "/" + nbNotes);
-			
-			if (nbUsers != null && (nbUsers.compareTo(0) > 0)) {			
-				idCreator = rand.nextInt(nbUsers - 1);
-				creator = "user" + idCreator;
+		if (nbUsers.compareTo(0) <= 0) {
+			logger.error("Notes can note be created without users.");
+		} else {
+			for (int i = 1; i <= nbNotes; i++) {
+				logger.info("Generating note " + i + "/" + nbNotes);
+
+				if (nbUsers != null && (nbUsers.compareTo(0) > 0)) {
+					idCreator = rand.nextInt(nbUsers - 1);
+					creator = "user" + idCreator;
+				}
+				ArticleBO article = articleService.getArticle(articles);
+				ContentGeneratorService.currentPageIndex++;
+				notes.add(new NoteBO("note" + i, "Note " + i, creator, article.getContent()));
 			}
-			ArticleBO article = articleService.getArticle(articles);
-			ContentGeneratorService.currentPageIndex++;
-			notes.add(new NoteBO("note" + i, "Note " + i, creator, article.getContent()));
 		}
-		
+
 		return notes;
 	}
 }
