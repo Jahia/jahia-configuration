6c675b3fca69fcba4419850a5dbc1d93853a878b
Quentin Lamérand <qlamerand@jahia.com>
2013-02-20 14:19:08 +0000

    BACKLOG-3538: Improve convertToOSGI goal : generate a minimal 'bundle' plugin, better move of src/main/webapp, and remove web.xml
    git-svn-id: file:///home/svn/repository/maven-jahia-plugin/trunk@677 2b131f5d-00a7-48c4-9630-96da95fee753

--- maven-jahia-plugin/src/main/java/org/jahia/utils/maven/plugin/ConvertToOSGI.java
+++ maven-jahia-plugin/src/main/java/org/jahia/utils/maven/plugin/ConvertToOSGI.java
@@ -11,49 +11,41 @@ import org.dom4j.io.OutputFormat;
 import org.dom4j.io.SAXReader;
 import org.dom4j.io.XMLWriter;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
 import java.util.*;
 
 /**
  * Jahia server deployment mojo.
+ *
  * @goal convertToOSGI
  * @requiresDependencyResolution runtime
  */
 public class ConvertToOSGI extends AbstractManagementMojo {
 
+    private static Map<String, String> jahiaManifestAttributes = new HashMap<String, String>();
+
+    static {
+        // depends, module-type and root-folder are not here to handle their default values
+        jahiaManifestAttributes.put("definitions", "Jahia-Definitions");
+        jahiaManifestAttributes.put("initial-imports", "Jahia-Initial-Imports");
+        jahiaManifestAttributes.put("resource-bundle", "Jahia-Resource-Bundle");
+        jahiaManifestAttributes.put("deploy-on-site", "Jahia-Deploy-On-Site");
+    }
+
     @Override
     public void doExecute() throws MojoExecutionException, MojoFailureException {
-        File webapp = new File(baseDir,"src/main/webapp");
-        File resources = new File(baseDir,"src/main/resources");
-        Set<String> rootFolders = new HashSet<String>();
+        File webapp = new File(baseDir, "src/main/webapp");
+        File resources = new File(baseDir, "src/main/resources");
         try {
             if (webapp.exists()) {
-                File[] files = webapp.listFiles(new FilenameFilter() {
-                    public boolean accept(File dir, String name) {
-                        return !name.startsWith(".");
-                    }
-                });
-
-                for (File file : files) {
-                    if (file.isDirectory()) {
-                        rootFolders.add(file.getName());
-                        FileUtils.moveDirectoryToDirectory(file, resources, true);
-                    } else {
-                        FileUtils.moveFileToDirectory(file, resources, true);
-                    }
-                }
+                FileUtils.deleteQuietly(new File(webapp, "WEB-INF/web.xml"));
+                moveWithMerge(webapp, resources);
             }
 
-            final Set<String> allNonEmptyDirectories = new HashSet<String>();
-            File directory = new File(baseDir, "src/main/java");
-            if (directory.exists() && directory.isDirectory()) {
-                Collection<File> packages = FileUtils.listFiles(directory, null, true);
-                for (File file : packages) {
-                    allNonEmptyDirectories.add(file.getPath().substring(directory.getPath().length()));
-                }
-            }
-
-            parsePom(allNonEmptyDirectories, rootFolders);
+            parsePom();
         } catch (DocumentException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         } catch (IOException e) {
@@ -61,7 +53,28 @@ public class ConvertToOSGI extends AbstractManagementMojo {
         }
     }
 
-    private void parsePom(Set<String> allNonEmptyDirectories, Set<String> rootFolders) throws DocumentException, IOException {
+    private void moveWithMerge(File src, File dst) throws IOException {
+        File[] files = src.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return !name.startsWith(".");
+            }
+        });
+
+        for (File file : files) {
+            if (file.isDirectory()) {
+                File subDir = new File(dst, file.getName());
+                if (subDir.exists()) {
+                    moveWithMerge(file, subDir);
+                } else {
+                    FileUtils.moveDirectoryToDirectory(file, dst, true);
+                }
+            } else {
+                FileUtils.moveFileToDirectory(file, dst, true);
+            }
+        }
+    }
+
+    private void parsePom() throws DocumentException, IOException {
         SAXReader reader = new SAXReader();
         File pom = new File(baseDir, "pom.xml");
         Document pomDocument = reader.read(pom);
@@ -95,9 +108,11 @@ public class ConvertToOSGI extends AbstractManagementMojo {
         Element pluginTemplate = (Element) bundleModuleDocument.selectSingleNode("/project/*[local-name()='build']/*[local-name()='plugins']/*[local-name()='plugin']");
         Element instructionsTemplate = (Element) pluginTemplate.element("configuration").element("instructions").detach();
         Element instructions = pluginTemplate.element("configuration").addElement("instructions");
-        plugins.add(pluginTemplate.detach());
 
-        xxxx(root, manifestEntries, instructions, instructionsTemplate, allNonEmptyDirectories, rootFolders);
+        generateBundlePlugin(manifestEntries, instructions, instructionsTemplate);
+        if (!instructions.elements().isEmpty()) {
+            plugins.add(pluginTemplate.detach());
+        }
 
         // Export pom
         XMLWriter writer = new XMLWriter(new FileOutputStream(pom), OutputFormat.createPrettyPrint());
@@ -105,139 +120,39 @@ public class ConvertToOSGI extends AbstractManagementMojo {
         writer.close();
     }
 
-    private void xxxx(Element root, Element manifestEntries, Element instructions, Element instructionsTemplate, Set<String> allNonEmptyDirectories, Set<String> rootFolders) {
-        List<String> classPathEntries = new ArrayList<String>();
+    private void generateBundlePlugin(Element manifestEntries, Element instructions, Element instructionsTemplate) {
 
-        if (classPathEntries.size() > 0) {
-            StringBuffer bundleClassPath = new StringBuffer(".");
-            for (String classPathEntry : classPathEntries) {
-                bundleClassPath.append(",");
-                bundleClassPath.append(classPathEntry);
-            }
-            instructions.addElement("Bundle-ClassPath").setText(bundleClassPath.toString());
+        Element depends = manifestEntries.element("depends");
+        String dependsStr = null;
+        if (depends != null) {
+            dependsStr = depends.getText();
+        }
+        if (dependsStr != null && !dependsStr.trim().equals("default") && !dependsStr.trim().equals("Default Jahia Templates")) {
+            instructions.addElement("Jahia-Depends").setText(dependsStr);
         }
 
-        Element version = root.element("version");
-        if (version == null) {
-            version = root.element("parent").element("version");
+        Element moduleType = manifestEntries.element("module-type");
+        String moduleTypeStr = null;
+        if (moduleType != null) {
+            moduleTypeStr = moduleType.getText();
         }
-        if (version != null) {
-            String versionStr = version.getText();
-            int dashPos = versionStr.indexOf("-");
-            if (dashPos > -1) {
-                versionStr = versionStr.substring(0, dashPos);
-            }
-            int underScorePos = versionStr.indexOf("_");
-            if (underScorePos > -1) {
-                versionStr = versionStr.substring(0, underScorePos);
-            }
-            instructions.addElement("Bundle-Version").setText(versionStr);
+        if (moduleTypeStr != null && !moduleTypeStr.equals("module")) {
+            instructions.addElement("Jahia-Module-Type").setText(moduleTypeStr);
         }
 
-        Set<String> exportPackageExcludes = new TreeSet<String>(new Comparator<String>() {
-            public int compare(String s, String s1) {
-                if (s.length() > s1.length()) {
-                    return -1;
-                } else if (s.length() < s1.length()) {
-                    return 1;
-                } else {
-                    return s.compareTo(s1);
-                }
-            }
-        });
-
-        // calculate export package exclusions that are all non-empty directories minus the ones that contain
-        // resources to export.
-        exportPackageExcludes.addAll(allNonEmptyDirectories);
-
-        String artifactId = root.element("artifactId").getText();
-        String rootFolder = artifactId;
-        String name = root.element("name").getText();
+        Element rootFolder = manifestEntries.element("root-folder");
+        String rootFolderStr = null;
         if (rootFolder != null) {
-            String packagePrefix = rootFolder.replaceAll("[ -]", "");
-
-            instructions.addElement("Bundle-SymbolicName").setText(rootFolder);
-            StringBuilder exportPackage = new StringBuilder("");
-//            if (exportPackageIncludes.size() > 0) {
-//                for (String exportPackageInclude : exportPackageIncludes) {
-//                    exportPackage.append(exportPackageInclude);
-//                    exportPackage.append(",");
-//                }
-//            }
-            /*
-            if (exportPackageExcludes.size() > 0) {
-                for (String exportPackageExclude : exportPackageExcludes) {
-                    exportPackage.append("!");
-                    exportPackage.append(exportPackageExclude);
-                    exportPackage.append(".*,");
-                }
-                //exportPackage.append("*,");
-            } else {
-                //exportPackage.append("*,");
-            }
-            */
-            exportPackage.append(name.replaceAll("[ -]", ""));
-            exportPackage.append(",");
-            exportPackage.append(packagePrefix);
-            instructions.addElement("Export-Package").setText(exportPackage.toString());
-
-            Element depends = manifestEntries.element("depends");
-            String dependsStr = "";
-            if (depends != null) {
-                dependsStr = depends.getText();
-            }
-            if (!dependsStr.contains("default") && !dependsStr.contains("Default Jahia Templates") && !rootFolder.equals("assets") && !rootFolder.equals("default")) {
-                if (!dependsStr.equals("")) {
-                    dependsStr += ",";
-                }
-                dependsStr += "default";
-            }
-
-
-            String[] dependsArray = dependsStr.split(",");
-            StringBuilder importPackage = new StringBuilder("*");
-
-            for (String dep : dependsArray) {
-                if (!"".equals(dep)) {
-                    importPackage.append(",");
-                    dep = dep.replaceAll("[ -]", "");
-                    importPackage.append(dep);
-                }
-            }
-
-            Set<String> templateImportPackages = new TreeSet<String>();
-            templateImportPackages.addAll(Arrays.asList(instructionsTemplate.element("Import-Package").getText().split(",")));
-
-            List<String> alreadyImportedPackages = new ArrayList<String>(Arrays.asList(importPackage.toString().split(",")));
-            for (String curImportPackage : templateImportPackages) {
-                if (!alreadyImportedPackages.contains(curImportPackage)) {
-                    importPackage.append(",");
-                    importPackage.append(curImportPackage);
-                    alreadyImportedPackages.add(curImportPackage);
-                }
-            }
-
-            instructions.addElement("Import-Package").setText(importPackage.toString());
-
-            StringBuilder staticResources = new StringBuilder("");
-            for (String folder : rootFolders) {
-                if (!folder.equals("META-INF") && !folder.equals("WEB-INF")) {
-                    staticResources.append(",/");
-                    staticResources.append(folder);
-                }
-            }
-            if (staticResources.length() > 0) {
-                instructions.addElement("Jahia-Static-Resources").setText(staticResources.substring(1));
-            }
+            rootFolderStr = rootFolder.getText();
+        }
+        if (rootFolderStr != null && !rootFolderStr.equals("${project.artifactId}")) {
+            instructions.addElement("Jahia-Root-Folder").setText(rootFolderStr);
         }
 
-        for (Element element : (Iterable<? extends Element>) instructionsTemplate.elements()) {
-            if (instructions.element(element.getName()) == null) {
-                instructions.add(element.detach());
-            }
-        }
-        for (Element element : (Iterable<? extends Element>) manifestEntries.elements()) {
-            if (instructions.element(element.getName()) == null) {
+        for (String name : jahiaManifestAttributes.keySet()) {
+            Element element = manifestEntries.element(name);
+            if (element != null) {
+                element.setName(jahiaManifestAttributes.get(name));
                 instructions.add(element.detach());
             }
         }
