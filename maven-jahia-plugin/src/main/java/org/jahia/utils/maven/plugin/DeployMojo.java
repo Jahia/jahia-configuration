60b11c380b1534920ba5ef4b60a11ae936470855
Sergiy Shyrkov <sshyrkov@jahia.com>
2013-02-25 21:06:04 +0000

    Added org.jahia.bundles.webconsole.config artifact ID to system bundles
    git-svn-id: file:///home/svn/repository/maven-jahia-plugin/trunk@686 2b131f5d-00a7-48c4-9630-96da95fee753

--- maven-jahia-plugin/src/main/java/org/jahia/utils/maven/plugin/DeployMojo.java
+++ maven-jahia-plugin/src/main/java/org/jahia/utils/maven/plugin/DeployMojo.java
@@ -91,7 +91,8 @@ public class DeployMojo extends AbstractManagementMojo {
     
     private static final Set<String> JAHIA_SYSTEM_BUNDLES = new HashSet<String>(Arrays.asList(
             "org.jahia.bundles.url.jahiawar", "org.jahia.bundles.extender.jahiamodules",
-            "org.jahia.bundles.blueprint.extender.config", "org.jahia.bundles.http.bridge"));
+            "org.jahia.bundles.blueprint.extender.config", "org.jahia.bundles.http.bridge",
+            "org.jahia.bundles.webconsole.config"));
 
     /**
      * The dependency tree builder to use.
