082729389eddb4d8e509ae1deff77e421badbac5
Quentin Lamérand <qlamerand@jahia.com>
2013-06-24 11:32:06 +0000

    BACKLOG-4012: Generate OSGi Config Admin LDAP properties
    git-svn-id: file:///home/svn/repository/maven-jahia-plugin/trunk@756 2b131f5d-00a7-48c4-9630-96da95fee753

--- configurators/src/test/java/org/jahia/configuration/configurators/LDAPConfiguratorTest.java
+++ configurators/src/test/java/org/jahia/configuration/configurators/LDAPConfiguratorTest.java
@@ -1,6 +1,8 @@
 package org.jahia.configuration.configurators;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
 import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 
@@ -12,7 +14,7 @@ import org.apache.commons.io.FileUtils;
  * Date: 27.04.11
  * Time: 17:22
  */
-public class LDAPConfiguratorTest extends AbstractXMLConfiguratorTestCase {
+public class LDAPConfiguratorTest extends AbstractConfiguratorTestCase {
     @Override
     public void testUpdateConfiguration() throws Exception {
         File targetFolder = new File(FileUtils.getTempDirectory(), "ldap-config-test-" + System.currentTimeMillis());
@@ -21,14 +23,11 @@ public class LDAPConfiguratorTest extends AbstractXMLConfiguratorTestCase {
             LDAPConfigurator ldapConfigurator = new LDAPConfigurator(oracleDBProperties, websphereOracleConfigBean);
             ldapConfigurator.updateConfiguration(null, targetFolder.getPath());
     
-            // @todo we should validate the generated WAR file here.
-            JarFile jarFile = new JarFile(new File(targetFolder, "ldap-config.jar"));
-            try {
-                Manifest manifest = jarFile.getManifest();
-                assertEquals("LDAP configuration WAR should depend on LDAP connector module", manifest.getMainAttributes().getValue("Jahia-Depends"), "ldap");
-            } finally {
-                jarFile.close();
-            }
+            Properties ldapProperties = new Properties();
+            ldapProperties.load(new FileInputStream(new File(targetFolder, "org.jahia.services.usermanager.ldap-config.cfg")));
+            assertEquals("ldap://10.8.37.17:389/", ldapProperties.getProperty("user.url"));
+            assertEquals("dc=jahia", ldapProperties.getProperty("group.search.name"));
+            assertEquals("givenName", ldapProperties.getProperty("user.j:firstName.attribute.map"));
         } finally {
             FileUtils.deleteDirectory(targetFolder);
         }
