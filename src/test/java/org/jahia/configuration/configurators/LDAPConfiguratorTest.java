5efa01292c0780696b9023a3e9e0f7015347cdec
Sergiy Shyrkov <sshyrkov@jahia.com>
2013-08-19 10:37:12 +0000

    Close streams
    git-svn-id: file:///home/svn/repository/maven-jahia-plugin/trunk@761 2b131f5d-00a7-48c4-9630-96da95fee753

--- src/test/java/org/jahia/configuration/configurators/LDAPConfiguratorTest.java
+++ src/test/java/org/jahia/configuration/configurators/LDAPConfiguratorTest.java
@@ -3,10 +3,9 @@ package org.jahia.configuration.configurators;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
-import java.util.jar.JarFile;
-import java.util.jar.Manifest;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 /**
  * Test unit for LDAP configurator
@@ -18,17 +17,19 @@ public class LDAPConfiguratorTest extends AbstractConfiguratorTestCase {
     @Override
     public void testUpdateConfiguration() throws Exception {
         File targetFolder = new File(FileUtils.getTempDirectory(), "ldap-config-test-" + System.currentTimeMillis());
-
+        FileInputStream inStream = null;
         try {
             LDAPConfigurator ldapConfigurator = new LDAPConfigurator(oracleDBProperties, websphereOracleConfigBean);
             ldapConfigurator.updateConfiguration(null, targetFolder.getPath());
     
             Properties ldapProperties = new Properties();
-            ldapProperties.load(new FileInputStream(new File(targetFolder, "org.jahia.services.usermanager.ldap-config.cfg")));
+            inStream = new FileInputStream(new File(targetFolder, "org.jahia.services.usermanager.ldap-config.cfg"));
+            ldapProperties.load(inStream);
             assertEquals("ldap://10.8.37.17:389/", ldapProperties.getProperty("user.url"));
             assertEquals("dc=jahia", ldapProperties.getProperty("group.search.name"));
             assertEquals("givenName", ldapProperties.getProperty("user.j:firstName.attribute.map"));
         } finally {
+            IOUtils.closeQuietly(inStream);
             FileUtils.deleteDirectory(targetFolder);
         }
     }
