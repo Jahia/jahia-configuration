c928829ac5f2ba76bbccb1976f9209c2146cd64c
Sergiy Shyrkov <sshyrkov@jahia.com>
2014-04-25 08:30:45 +0000

    Quote values for JBoss datasource configuration
    git-svn-id: file:///home/svn/repository/maven-jahia-plugin/trunk@877 2b131f5d-00a7-48c4-9630-96da95fee753

--- src/main/java/org/jahia/configuration/configurators/JBossConfigurator.java
+++ src/main/java/org/jahia/configuration/configurators/JBossConfigurator.java
@@ -273,14 +273,14 @@ public class JBossConfigurator extends AbstractXMLConfigurator {
         // add datasource configuration
         cli.append("data-source add --name=jahiaDS --jndi-name=java:/jahiaDS --enabled=true --use-java-context=true \\\n");
         cli.append("--driver-name=jahia.").append(dbType).append(" \\\n");
-        cli.append("--connection-url=").append(getDbPropForCLI("jahia.database.url")).append(" \\\n");
+        cli.append("--connection-url=\"").append(getDbPropForCLI("jahia.database.url")).append("\" \\\n");
         String v = getDbPropForCLI("jahia.database.user");
         if (v != null && v.length() > 0) {
-            cli.append("--user-name=").append(v).append(" \\\n");
+            cli.append("--user-name=\"").append(v).append("\" \\\n");
         }
         v = getDbPropForCLI("jahia.database.pass");
         if (v != null && v.length() > 0) {
-            cli.append("--password=").append(v).append(" \\\n");
+            cli.append("--password=\"").append(v).append("\" \\\n");
         }
 
         cli.append("--min-pool-size=" + MIN_POOL_SIZE + " \\\n");
