1f5fee0b46ce768d60ef8cf09bd77747020af2ff
Sergiy Shyrkov <sshyrkov@jahia.com>
2014-07-03 11:38:09 +0000

    Properly close VFS resources (especially JAR files)
    git-svn-id: file:///home/svn/repository/maven-jahia-plugin/trunk@904 2b131f5d-00a7-48c4-9630-96da95fee753

--- src/main/java/org/jahia/configuration/configurators/VFSConfigFile.java
+++ src/main/java/org/jahia/configuration/configurators/VFSConfigFile.java
@@ -4,14 +4,33 @@ import org.apache.commons.vfs.FileObject;
 import org.apache.commons.vfs.FileSystemException;
 import org.apache.commons.vfs.FileSystemManager;
 
+import java.io.Closeable;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Implementation of the ConfigFile interface that uses Common VFS to provide the internal functionality.
  */
-public class VFSConfigFile implements ConfigFile {
+public class VFSConfigFile implements ConfigFile, Closeable {
+    
+    private static Set<Closeable> OPENED = new HashSet<Closeable>();
+    
+    public static synchronized void closeAllOpened() {
+        for (Iterator<Closeable> iterator = OPENED.iterator(); iterator.hasNext();) {
+            try {
+                iterator.next().close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                iterator.remove();
+            }
+        }
+    }
 
     private FileObject fileObject;
 
@@ -19,19 +38,30 @@ public class VFSConfigFile implements ConfigFile {
         this.fileObject = fileObject;
     }
 
-    public VFSConfigFile(FileSystemManager fileSystemManager, String uri) throws FileSystemException {
-        this.fileObject = fileSystemManager.resolveFile(uri);
-    }
-
     public VFSConfigFile(FileObject parentFileObject, String path) throws FileSystemException {
         this.fileObject = parentFileObject.resolveFile(path);
     }
 
-    public URI getURI() throws FileSystemException, URISyntaxException {
-        return fileObject.getURL().toURI();
+    public VFSConfigFile(FileSystemManager fileSystemManager, String uri) throws FileSystemException {
+        this.fileObject = fileSystemManager.resolveFile(uri);
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (fileObject != null) {
+            fileObject.close();
+            fileObject.getFileSystem().getFileSystemManager().closeFileSystem(fileObject.getFileSystem());
+        }
     }
 
     public InputStream getInputStream() throws FileSystemException {
+        synchronized (OPENED) { 
+            OPENED.add(this);
+        }
         return fileObject.getContent().getInputStream();
+    }
+
+    public URI getURI() throws FileSystemException, URISyntaxException {
+        return fileObject.getURL().toURI();
     }
 }
